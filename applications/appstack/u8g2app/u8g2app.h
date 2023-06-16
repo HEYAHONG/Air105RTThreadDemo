737b58242c35d044b284762915aa55ce56e84e2d
HEYAHONG <2229388563@qq.com>
2023-06-16 14:11:39 +0800

    添加边框

--- applications/appstack/u8g2app/u8g2app.h
+++ applications/appstack/u8g2app/u8g2app.h
@@ -52,38 +52,45 @@ private:
         //注意:传入的event不可为空
         display=event;
 
+
+
         //显示启动中字样
         display->setFont(u8g2_font_wqy12_t_gb2312);
         display->clearBuffer();
-        display->setCursor(0,32);
+        display->drawFrame(0,0,128,64);
+        display->setCursor(5,32);
         display->print("启动中...");
         display->sendBuffer();
         msleep(400);
 
         display->setFont(u8g2_font_wqy13_t_gb2312);
         display->clearBuffer();
-        display->setCursor(0,32);
+        display->drawFrame(0,0,128,64);
+        display->setCursor(5,32);
         display->print("启动中...");
         display->sendBuffer();
         msleep(400);
 
         display->setFont(u8g2_font_wqy14_t_gb2312);
         display->clearBuffer();
-        display->setCursor(0,32);
+        display->drawFrame(0,0,128,64);
+        display->setCursor(5,32);
         display->print("启动中...");
         display->sendBuffer();
         msleep(400);
 
         display->setFont(u8g2_font_wqy15_t_gb2312);
         display->clearBuffer();
-        display->setCursor(0,32);
+        display->drawFrame(0,0,128,64);
+        display->setCursor(5,32);
         display->print("启动中...");
         display->sendBuffer();
         msleep(400);
 
         display->setFont(u8g2_font_wqy16_t_gb2312);
         display->clearBuffer();
-        display->setCursor(0,32);
+        display->drawFrame(0,0,128,64);
+        display->setCursor(5,32);
         display->print("启动中...");
         display->sendBuffer();
 
